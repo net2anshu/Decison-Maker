202fa777a051130ea024ee4f7f48a915df5df33b
anderdm <ademiguelaramburu@gmail.com>
2020-01-28 19:21:24 +0100

    working

--- app/src/main/java/com/mobisec/DecisionMaker/ChooseActivity.java
+++ app/src/main/java/com/mobisec/DecisionMaker/ChooseActivity.java
@@ -6,10 +6,18 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mobisec.DecisionMaker.model.EventActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChooseActivity extends Activity {
+
+    private List<EventActivity> activities;
 
 
     @Override
@@ -18,6 +26,13 @@ public class ChooseActivity extends Activity {
         setContentView(R.layout.activity_main);
         Button btn_confirm = (Button)findViewById(R.id.confirmButton);
         final TextView EventName = findViewById(R.id.title);
+
+        activities = new ArrayList<>();
+
+        AddActivityListAdapter adapter = new AddActivityListAdapter(this, activities);
+
+        ListView listView = findViewById(R.id.listActivities);
+        listView.setAdapter(adapter);
 
 
 
