8a3a04557486f89c6e31384d737f35bf1b45ccf6
Vahur Varris <varrisvahur@gmail.com>
2020-01-28 12:07:34 +0100

    Created new event activity branch

--- app/src/main/java/com/mobisec/DecisionMaker/MainActivity.java
+++ app/src/main/java/com/mobisec/DecisionMaker/MainActivity.java
@@ -1,8 +1,8 @@
 package com.mobisec.DecisionMaker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
